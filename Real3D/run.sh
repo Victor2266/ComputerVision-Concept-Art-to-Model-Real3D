53529a7fcbbf1a8b23758a70504e57dbb35f455d
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2024-11-18 16:57:51 -0500

    initial commit

---
+++ Real3D/run.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 python run.py ./assets/examples/preview3.png --output-dir output_demo --render --render-num-views 72 --model-save-format glb --mc-resolution 256 #--no-remove-bg 
