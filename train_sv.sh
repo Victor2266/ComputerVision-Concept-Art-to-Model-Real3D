61d71bd5bac54c82a9623d2a38e2ae0ecbd82836
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2024-11-23 14:50:50 -0500

    modified training script to use 1 gpu instead of 8

--- train_sv.sh
+++ train_sv.sh
@@ -1,5 +1,5 @@
 # this is slow but might be more accurate
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --master_port 8888 --nproc_per_node=8 \
+CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --master_port 8888 --nproc_per_node=1 \
 train.py --cfg ./config/objaverse_both_all/train_sv-novel_consistency-curriculum-rerender.yaml
 
 # this is faster but might be a bit less accurate
