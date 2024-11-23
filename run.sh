e032675ea4dc390d40d07eda16d3a3e3586feaf7
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2024-11-23 18:04:07 -0500

    changed file permission for run script

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
 CUDA_VISIBLE_DEVICES=0 python run.py ./assets/examples/preview3.png --output-dir output_demo --render --render-num-views 72 --model-save-format glb --mc-resolution 256 #--no-remove-bg 
+
+#if you get permission error then run chmod +x run.sh
