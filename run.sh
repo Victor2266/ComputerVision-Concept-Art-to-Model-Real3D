b759101089aa0d81cb8cdfd3741df2ab895ba468
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2024-11-24 00:46:10 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
+# If you get a permission error trying to run ./run.sh then run: chmod +x run.sh
+
+
+#This Runs the Default Model
 CUDA_VISIBLE_DEVICES=0 python run.py './assets/examples/GO_Bus.jpg' --output-dir output_demo --render --render-num-views 72 --model-save-format glb --mc-resolution 256 --pretrained-model-name-or-path './checkpoint/model_both_trained_v1.ckpt' #--no-remove-bg 
 
-#if you get permission error then run chmod +x run.sh
+
+# This one will run our Sword Model
+# CUDA_VISIBLE_DEVICES=0 python run.py './assets/examples/GO_Bus.jpg' --output-dir output_demo --render --render-num-views 72 --model-save-format glb --mc-resolution 256 --pretrained-model-name-or-path './checkpoint/sword_model_v1.ckpt' #--no-remove-bg 
