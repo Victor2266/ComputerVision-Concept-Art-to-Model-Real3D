4a71f6b42a3314ff8c94c88de5916fd1ed145aa1
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2024-11-29 01:17:59 -0500

    add apple

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 # If you get a permission error trying to run ./run.sh then run: chmod +x run.sh
-
-
 #This Runs the Default Model
-CUDA_VISIBLE_DEVICES=0 python run.py './assets/examples/GO_Bus.jpg' --output-dir output_demo --render --render-num-views 72 --model-save-format glb --mc-resolution 256 --pretrained-model-name-or-path './checkpoint/model_both_trained_v1.ckpt' #--no-remove-bg 
+CUDA_VISIBLE_DEVICES=0 python run.py './assets/examples/apple.jpg' --output-dir output_demo --render --render-num-views 72 --model-save-format glb --mc-resolution 256 --pretrained-model-name-or-path './checkpoint/new_fruit_model_3000k.ckpt' #--no-remove-bg 
 
 
 # This one will run our Sword Model
