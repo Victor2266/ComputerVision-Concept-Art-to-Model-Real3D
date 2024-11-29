0805af63f7b5ef07d5241e97b8c3973d143f8e20
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2024-11-29 15:53:48 -0500

    remove empty lines

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # If you get a permission error trying to run ./run.sh then run: chmod +x run.sh
 #This Runs the Default Model
 CUDA_VISIBLE_DEVICES=0 python run.py './assets/examples/apple.jpg' --output-dir output_demo --render --render-num-views 72 --model-save-format glb --mc-resolution 256 --pretrained-model-name-or-path './checkpoint/model_both_trained_v1.ckpt' #--no-remove-bg 
-
-
+#
+#
 # This one will run our Sword Model
 # CUDA_VISIBLE_DEVICES=0 python run.py './assets/examples/GO_Bus.jpg' --output-dir output_demo --render --render-num-views 72 --model-save-format glb --mc-resolution 256 --pretrained-model-name-or-path './checkpoint/sword_model_v1.ckpt' #--no-remove-bg 
